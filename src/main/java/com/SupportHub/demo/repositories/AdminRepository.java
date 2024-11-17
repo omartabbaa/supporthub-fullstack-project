8d33b43284f07fa753bb79eed969d801697f88fd
omartabbaa <omartabbaa0@gmail.com>
2024-11-17 16:30:04 +0100

    Added method to find QuestionLikes by Question and User in QuestionLikesRepository , Added method to find Admin by userId in AdminRepository

--- src/main/java/com/SupportHub/demo/repositories/AdminRepository.java
+++ src/main/java/com/SupportHub/demo/repositories/AdminRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.SupportHub.demo.models.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
+    Admin findByUser_UserId(Long userId);
 }
