db198fd3167b762d383086ba5ce7c5a13ad836a6
omartabbaa <omartabbaa0@gmail.com>
2024-11-18 18:07:34 +0100

    bussness dtos

--- src/main/java/com/SupportHub/demo/dtos/OutputDTOs/BusinessOutputDTO.java
+++ src/main/java/com/SupportHub/demo/dtos/OutputDTOs/BusinessOutputDTO.java
@@ -1,5 +1,41 @@
 package com.SupportHub.demo.dtos.OutputDTOs;
 
 public class BusinessOutputDTO {
-    
+    private Long businessId;
+    private String name;
+    private String description;
+    private String logo;
+
+    // Getters and Setters
+    public Long getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(Long businessId) {
+        this.businessId = businessId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
 }
