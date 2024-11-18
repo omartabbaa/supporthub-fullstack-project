7c6cdeae6bbad2b4ba5c8ba0d4964d6fdee3f496
omartabbaa <omartabbaa0@gmail.com>
2024-11-18 20:40:29 +0100

    permission mapper/dtos

--- src/main/java/com/SupportHub/demo/dtos/InputDTOs/PermissionInputDTO.java
+++ src/main/java/com/SupportHub/demo/dtos/InputDTOs/PermissionInputDTO.java
@@ -1,5 +1,41 @@
 package com.SupportHub.demo.dtos.InputDTOs;
 
 public class PermissionInputDTO {
-    
+    private Long userId;
+    private Long departmentId;
+    private Long projectId;
+    private Boolean canAnswer;
+
+    // Getters and Setters
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public Boolean getCanAnswer() {
+        return canAnswer;
+    }
+
+    public void setCanAnswer(Boolean canAnswer) {
+        this.canAnswer = canAnswer;
+    }
 }
