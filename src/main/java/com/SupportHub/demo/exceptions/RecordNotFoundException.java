b61c65b9d97efee4d093da948ce9a9a023e00c7f
omartabbaa <omartabbaa0@gmail.com>
2024-11-19 20:52:43 +0100

    security jwt

---
+++ src/main/java/com/SupportHub/demo/exceptions/RecordNotFoundException.java
@@ -0,0 +1,13 @@
+package com.SupportHub.demo.exceptions;
+
+public class RecordNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public RecordNotFoundException(){
+        super();
+    }
+
+    public RecordNotFoundException(String message){
+        super(message);
+    }
+}
